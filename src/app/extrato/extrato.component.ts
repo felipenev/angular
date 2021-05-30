e66dc72b418bf8053f5ca7677f476ee4e9d0987d
Felipe Neves <matosfnm@gmail.com>
2021-05-30 14:04:13 -0300

    Services

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
   templateUrl: './extrato.component.html',
-  styleUrls: ['./extrato.component.scss']
+  styleUrls: ['./extrato.component.scss'],
 })
 export class ExtratoComponent implements OnInit {
+  @Input() transferencias: any[];
 
-  constructor() { }
+  constructor(private service: TransferenciaService) {}
 
   ngOnInit(): void {
+    this.transferencias = this.service.transferencias;
   }
-
 }
